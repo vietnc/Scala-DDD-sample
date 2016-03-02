4439d15b4937e0c392f598189f7b136fc314926c
viet_nc <viet_nc@septeni-technology.jp>
2016-03-02 13:01:23 +0700

    improve coding with Try

--- app/sep/com/bbs/domain/model/Article/ArticleRepositoryImpl.scala
+++ app/sep/com/bbs/domain/model/Article/ArticleRepositoryImpl.scala
@@ -7,22 +7,18 @@ import sep.com.bbs.infra.ArticleDAO
 import scala.util.Try
 class ArticleRepositoryImpl extends ArticleRepository{
 
-  def resolveById(id: ArticleID): Try[Option[Article]] = {
-    Try(
+  def resolveById(id: ArticleID): Try[Option[Article]] = Try {
       ArticleDAO.findById(id.value) match {
       case Some(dto) => Some(ArticleDomainService.loadDTO(dto))
       case _ => None
-    })
-
+    }
   }
 
-  def resolveAll(): Try[List[Article]] = {
-    Try(
-      ArticleDAO.findAll().map(articleInfo => ArticleDomainService.loadDTO(articleInfo)))
+  def resolveAll(): Try[List[Article]] = Try {
+      ArticleDAO.findAll().map(articleInfo => ArticleDomainService.loadDTO(articleInfo))
   }
 
-  def store(article: Article): Try[Boolean] = {
-    Try(
-      ArticleDAO.save(ArticleDomainService.getDTO(article)))
+  def store(article: Article): Try[Boolean] = Try {
+      ArticleDAO.save(ArticleDomainService.getDTO(article))
   }
 }
--- app/sep/com/bbs/domain/model/User/UserRepositoryImpl.scala
+++ app/sep/com/bbs/domain/model/User/UserRepositoryImpl.scala
@@ -5,20 +5,19 @@ import sep.com.bbs.domain.shared._
 import scala.util.Try
 class UserRepositoryImpl extends UserRepository{
 
-    def findByEmail(email: String): Try[Option[User]] = {
-      Try(None)
+    def findByEmail(email: String): Try[Option[User]] = Try {
+      None
     }
 
-    def resolveById(id: UserID): Try[Option[User]] = {
-      Try(None)
-
+    def resolveById(id: UserID): Try[Option[User]] = Try {
+      None
     }
-    def resolveAll(): Try[List[User]] = {
-      Try(List())
+    def resolveAll(): Try[List[User]] = Try {
+      List()
     }
 
-    def store(user: User): Try[Boolean] = {
-      Try(true)
+    def store(user: User): Try[Boolean] = Try {
+      true
     }
 
 
