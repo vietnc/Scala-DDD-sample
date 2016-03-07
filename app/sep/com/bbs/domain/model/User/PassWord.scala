b341b51eed86e7d203fe6dab839a8d114c3b368a
viet_nc <viet_nc@septeni-technology.jp>
2016-03-07 17:37:46 +0700

    updated about Password

--- app/sep/com/bbs/domain/model/User/PassWord.scala
+++ app/sep/com/bbs/domain/model/User/PassWord.scala
@@ -3,9 +3,8 @@ package sep.com.bbs.domain.model.user
 import sep.com.bbs.domain.shared.ValueObject
 import sep.com.bbs.infra.util.BbsSecure
 
-case class PassWord(raw: String, hashed: String) extends ValueObject
+case class PassWord( hashed: String) extends ValueObject
 
 object PassWord{
-  def apply(hashStr:String):PassWord = PassWord("",hashStr)
-  def fromRaw(raw: String):PassWord  = PassWord(raw,BbsSecure.hash(raw))
+  def fromRaw(raw: String):PassWord  = PassWord(BbsSecure.hash(raw))
 }
